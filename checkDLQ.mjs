d754d23dcb9d4c8e828ae675d03689addd7ecb98
Vijay <vijaydharani269@gmail.com>
2025-11-10 00:21:21 +0530

    Queuectl Done

--- checkDLQ.mjs
+++
@@ -1,9 +0,0 @@
-import { getDB, initDB } from './src/db/database.js';
-
-await initDB();
-
-const db = getDB();
-await db.read();
-
-console.log('Jobs:', db.data.jobs);
-console.log('DLQ:', db.data.dlq);
